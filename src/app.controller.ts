68a5ba41e9449d109cade59ff58d484fa564077f
Ramiro <ramifazio@gmail.com>
2023-09-03 18:30:45 -0300

    @Get endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from './data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('id') id: string, @Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
